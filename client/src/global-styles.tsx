3956021088468b1eb41f32632d100b66d2e1bb0e
darktell <olegbelinski1399@gmail.com>
2021-10-26 11:03:56 +0300

    fix global style

--- client/src/global-styles.tsx
+++ client/src/global-styles.tsx
@@ -3,9 +3,9 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;  
-        font-family: "Roboto", sans-serif;
-        font-weight: 500;
-        background-color: aliceblue;
-        overflow-y: hidden;
+    font-family: "Roboto", sans-serif;
+    font-weight: 500;
+    background-color: aliceblue;
+    overflow-y: hidden;
   }
 `;
