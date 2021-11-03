38dfeb2c48377155618e7de9dcede2fc0ac42bea
darktell <olegbelinski1399@gmail.com>
2021-11-03 11:52:28 +0200

    update regular expressions

--- client/src/utils/validators/RegularExpressions.ts
+++ client/src/utils/validators/RegularExpressions.ts
@@ -1,4 +1,3 @@
-/*eslint-disable-next-line*/
 export const emailRE = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 export const passwordSpacesRE = /(\s)/g;
 export const passwordCyrillicRE = /[^a-zA-Z0-9]/g;
