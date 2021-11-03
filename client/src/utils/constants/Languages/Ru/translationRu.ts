312c90418bfbd907b19e4ee3af24c7542aad7a67
darktell <olegbelinski1399@gmail.com>
2021-11-03 11:40:17 +0200

    update regular expressions

--- client/src/utils/constants/Languages/Ru/translationRu.ts
+++ client/src/utils/constants/Languages/Ru/translationRu.ts
@@ -1,75 +1,78 @@
 export const ru = {
-    // Side menu 
-    'Dashboard': 'Домашняя страница',
-    'Users': 'Пользователи',
-    'Pages': 'Страницы',
-    'Modules': 'Модули',
-    'Groups': 'Группы',
-    'Newsletter': 'Новостная рассылка',
-    'Webshop': 'Интернет магазин',
-    'Settings': 'Настройки',
+    // Side menu
+    Dashboard: 'Домашняя страница',
+    Users: 'Пользователи',
+    Pages: 'Страницы',
+    Modules: 'Модули',
+    Groups: 'Группы',
+    Newsletter: 'Новостная рассылка',
+    Webshop: 'Интернет магазин',
+    Settings: 'Настройки',
     //Sub items side menu
     //Pages
-    'All Pages':'Все страницы',
-    'New Pages':'Новая страница',
-    'Edit Page':'Редактировать страницу',
+    'All Pages': 'Все страницы',
+    'New Pages': 'Новая страница',
+    'Edit Page': 'Редактировать страницу',
     //NewsLetter
-    'All Letter':'Все письма',
-    'All Jobs':'Все рассылки',
+    'All Letter': 'Все письма',
+    'All Jobs': 'Все рассылки',
     //Webshop
-    'Categories': 'Категории',
-    'All Fields':'Поля',
-    'All Items':'Продукты',
-    'All Orders':'Заказы',
-    // Settings 
-    'Language': 'Язык',
+    Categories: 'Категории',
+    'All Fields': 'Поля',
+    'All Items': 'Продукты',
+    'All Orders': 'Заказы',
+    // Settings
+    Language: 'Язык',
 
     //Actions
-    'Logout':'Выйти',
-    'Yes':'Да',
-    'No':'Нет',
+    Logout: 'Выйти',
+    Yes: 'Да',
+    No: 'Нет',
 
     //List
-    'Name':'Имя',
-    'Email':'Почта',
-    'Number':'Номер',
-    'interact':'Действие',
+    Name: 'Имя',
+    Email: 'Почта',
+    Number: 'Номер',
+    interact: 'Действие',
     //Modal message
-    'Are you sure you want to log out?':'Вы уверенны что хотите покинуть сайт ?',
-    'A confirmation letter has been sent to the Email !':'Письмо с подтверждением отправлено на Email !',
-    'Password reset successfuly !':'Пароль сброшен успешно !',
+    'Are you sure you want to log out?': 'Вы уверенны что хотите покинуть сайт ?',
+    'A confirmation letter has been sent to the Email !':
+        'Письмо с подтверждением отправлено на Email !',
+    'Password reset successfuly !': 'Пароль сброшен успешно !',
 
-    //Auth 
-    'Forgot Password':'Забыл пароль',
-    'Sign In':'Войти',
-    'Register':'Зарегистрироваться',
-    'Don`t have an account ?':'Нет аккаунта ?',
-    'Hello ! Welcome back.':'Привет ! Добро пожаловать назад.',
-    'Log in with your data that you entered during Your registrarion':'Авторизуйтесь, используя данные, которые вы ввели при регистрации.',
-    'Have an account ?':'Уже есть аккаунт?',
-    'Create new account':'Создать новый аккаунт',
-    'Enter with your data details to create a new account':'Введите свои данные, чтобы создать новую учетную запись',
-    'Sign Up':'Зарегистрироваться',
-    'Log in':'Войти',
-    'Reset Password':'Сброс пароля',
-    'To reset your password enter your email':'Для сброса пароля введите свой адрес электронной почты',
-    'Send email':'Отправить письмо',
-    'To reset your password Enter new password':'Для сброса пароля Введите новый пароль',
-
+    //Auth
+    'Forgot Password': 'Забыл пароль',
+    'Sign In': 'Войти',
+    Register: 'Зарегистрироваться',
+    'Don`t have an account ?': 'Нет аккаунта ?',
+    'Hello ! Welcome back.': 'Привет ! Добро пожаловать назад.',
+    'Log in with your data that you entered during Your registrarion':
+        'Авторизуйтесь, используя данные, которые вы ввели при регистрации.',
+    'Have an account ?': 'Уже есть аккаунт?',
+    'Create new account': 'Создать новый аккаунт',
+    'Enter with your data details to create a new account':
+        'Введите свои данные, чтобы создать новую учетную запись',
+    'Sign Up': 'Зарегистрироваться',
+    'Log in': 'Войти',
+    'Reset Password': 'Сброс пароля',
+    'To reset your password enter your email':
+        'Для сброса пароля введите свой адрес электронной почты',
+    'Send email': 'Отправить письмо',
+    'To reset your password Enter new password': 'Для сброса пароля Введите новый пароль',
 
     //Validate
-    'Wrong password!':'Неправильный пароль!',
-    'Required':'Обязательное поле',
-    'Invalid email address':'Неверный адрес электронной почты',
-    'Password must not contain spaces':'Пароль не должен содержать пробелов',
-    'Minimum 8 characters':'Минимум 8 символов',
-    'Password must not contain cyrillic':'Пароль не должен содержать кириллицы.',
-    'Passwords must match':'Пароли должны совпадать',
-    'Must have a maximum of 30 characters':'Должно быть не более 30 символов.',
-    'The first name must contain only letters':'Имя должно состоять только из букв',
-    'First letter must be in upper case':'Первая буква должна быть в верхнем регистре',
-    'The last name must contain only letters':'Фамилия должна состоять только из букв',
-    'The phone number must contain only numbers':'Номер телефона должен содержать только цифры',
-    'Invalid Phone!':'Неверный телефон',
-    'Wrong date':'Неправильная дата'
+    'Wrong password!': 'Неправильный пароль!',
+    Required: 'Обязательное поле',
+    'Invalid email address': 'Неверный адрес электронной почты',
+    'Password must not contain spaces': 'Пароль не должен содержать пробелов',
+    'Minimum 8 characters': 'Минимум 8 символов',
+    'Password must not contain cyrillic': 'Пароль не должен содержать кириллицы.',
+    'Passwords must match': 'Пароли должны совпадать',
+    'Must have a maximum of 30 characters': 'Должно быть не более 30 символов.',
+    'The first name must contain only letters': 'Имя должно состоять только из букв',
+    'First letter must be in upper case': 'Первая буква должна быть в верхнем регистре',
+    'The last name must contain only letters': 'Фамилия должна состоять только из букв',
+    'The phone number must contain only numbers': 'Номер телефона должен содержать только цифры',
+    'Invalid Phone!': 'Неверный телефон',
+    'Wrong date': 'Неправильная дата'
 };
--- client/src/utils/validators/RegularExpressions.ts
+++ client/src/utils/validators/RegularExpressions.ts
@@ -1,4 +1,4 @@
-export const emailRE =
-    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+/*eslint-disable-next-line*/
+export const emailRE = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 export const passwordSpacesRE = /(\s)/g;
 export const passwordCyrillicRE = /[^a-zA-Z0-9]/g;
